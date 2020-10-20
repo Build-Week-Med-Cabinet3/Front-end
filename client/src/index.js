84f76b5c3c8b65a2e8ba608ab40a330af6d26e24
KVlearn <kavya.mdu@gmail.com>
2020-10-20 08:21:35 -0700

    route in index is commented

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,9 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    {/* <Router> */}
     <App />
-    </Router>
+    {/* </Router> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
